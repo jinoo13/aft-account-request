88935aa1c7cb3ef058b4725f8dc2c22ec427fe46
jinoo13 <falconet016@gmail.com>
2025-12-12 15:37:57 +0900

    first account request

---
+++ terraform/account-requests.tf
@@ -0,0 +1,27 @@
+module "account_request_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "etechaws+afttest1@gmail.com"
+    AccountName               = "afttest1"
+    ManagedOrganizationalUnit = "Sandbox (ou-qifn-c3aogld5)" 
+    SSOUserEmail              = "etechaws+afttest1@gmail.com"
+    SSOUserFirstName          = "test1"
+    SSOUserLastName           = "aft"
+  }
+
+  account_tags = {
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
